6d84cef12685cd5679f16230e9a3eaf1152602d1
Hyowon83 <aile1383@gmail.com>
2021-12-23 15:45:00 +0900

    03. 컴포넌트 변수 설정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
 
-class Subject extends Component {
-  render() {
-    return (
-      //반드시 하나의 최상위 태그만 써야한다.
-      <header>
-          <h1>WEB</h1>
-          world wide web!
-      </header>
-    );
-  }
-}
+// class Subject extends Component {
+//   render() {
+//     return (
+//       //반드시 하나의 최상위 태그만 써야한다.
+//       <header>
+//           <h1>WEB</h1>
+//           world wide web!
+//       </header>
+//     );
+//   }
+// }
 
 class TOC extends Component {
   render () {
@@ -27,12 +27,35 @@ class TOC extends Component {
   }
 }
 
+// class Content extends Component {
+//   render () {
+//     return (
+//       <article>
+//           <h2>HTML</h2>
+//           HTML is HyperText Markup Language.
+//       </article>
+//     );
+//   }
+// }
+
+//재사용성을 위한 컴포넌트로 바꾸기
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+          <h1>{this.props.title}</h1>
+          {this.props.sub}
+      </header>
+    );
+  }
+}
+
 class Content extends Component {
   render () {
     return (
       <article>
-          <h2>HTML</h2>
-          HTML is HyperText Markup Language.
+          <h2>{this.props.title}</h2>
+          {this.props.desc}
       </article>
     );
   }
@@ -42,9 +65,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        {/* <Subject></Subject> */}
+        <Subject title = "WEB" sub = "world wide web!!!"></Subject>
+        <Subject title = "React" sub = "For UI"></Subject>
+        <Subject title = "Jang Hyowon" sub = "PLM Solution Engineer"></Subject>
         <TOC></TOC>
-        <Content></Content>
+        {/* <Content></Content> */}
+        <Content title = "HTML" desc = "HTML is HyperText Markup Language."></Content>
+        <Content title = "Genergy" desc = "PLM is Product Lifecycle Management."></Content>
       </div>
     );
   }
