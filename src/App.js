ddf9fcdf506e5d7ea2a0bba528140825e16a8098
Hyowon83 <aile1383@gmail.com>
2021-12-23 16:28:44 +0900

    04. 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import Subject from './components/Subject';
+import Content from './components/Content';
+import TOC from './components/TOC';
 import './App.css';
 
 // class Subject extends Component {
@@ -13,20 +16,6 @@ import './App.css';
 //   }
 // }
 
-class TOC extends Component {
-  render () {
-    return (
-      <nav>
-          <ul>
-              <li><a href="1.html">HTML</a></li>
-              <li><a href="2.html">CSS</a></li>
-              <li><a href="3.html">JavaScript</a></li>
-          </ul>
-      </nav>
-    );
-  }
-}
-
 // class Content extends Component {
 //   render () {
 //     return (
@@ -39,27 +28,18 @@ class TOC extends Component {
 // }
 
 //재사용성을 위한 컴포넌트로 바꾸기
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-          <h1>{this.props.title}</h1>
-          {this.props.sub}
-      </header>
-    );
-  }
-}
+// class Subject extends Component {
+//   render() {
+//     return (
+//       <header>
+//           <h1>{this.props.title}</h1>
+//           {this.props.sub}
+//       </header>
+//     );
+//   }
+// }
 
-class Content extends Component {
-  render () {
-    return (
-      <article>
-          <h2>{this.props.title}</h2>
-          {this.props.desc}
-      </article>
-    );
-  }
-}
+
 
 class App extends Component {
   render() {
